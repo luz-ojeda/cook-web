8d3525266ef6bf9596b53259fdb61595ac3acf92
Luz Ojeda <luzojeda@proton.me>
2024-04-02 15:59:00 -0300

    Develop (#33)
    * Close burgermenu after search
    
    * Set instructions white/space paragraph to pre-line to preserve newline characters
    
    * Sett the x-api-key header on the api calls
    
    * Update security headers sent in hooks.server.ts and update Dockerfile with api key

--- Dockerfile
+++ Dockerfile
@@ -26,7 +26,7 @@ RUN npm ci --include=dev
 # Copy application code
 COPY --link . .
 
-RUN --mount=type=secret,id=API_URL API_URL="$(cat /run/secrets/API_URL)"
+RUN --mount=type=secret,id=API_URL --mount=type=secret,id=API_KEY API_URL="$(cat /run/secrets/API_URL)" API_KEY="$(cat /run/secrets/API_KEY)"
 
 ENV PUBLIC_AZURE_STORAGE_SAS_TOKEN='sp=r&st=2024-03-18T01:19:57Z&se=2024-05-16T09:19:57Z&spr=https&sv=2022-11-02&sr=c&sig=UH5PP4XKr9wlqRyTASqVUEAWhCi%2BOhEoZ%2F5YcbVjaYo%3D'
 
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,17 @@
-import type { HandleServerError } from '@sveltejs/kit';
+import type { Handle, HandleServerError } from '@sveltejs/kit';
 import { logRecipesError } from './errorLogging';
 import { MESSAGES } from './constants';
 import type { ApiError } from '$lib/types/ApiError';
+
+const securityHeaders = {
+    'Cross-Origin-Embedder-Policy': 'require-corp',
+    'Cross-Origin-Opener-Policy': 'same-origin',
+	'Strict-Transport-Security': 'max-age=31536000',
+    // [...],
+	'Referrer-Policy': 'no-referrer-when-downgrade',
+	'X-Content-Type-Options': 'nosnif',
+	'X-Frame-Options': 'DENY'
+}
 
 export const handleError: HandleServerError = async ({ event, error }) => {
 	if (event.route.id === '/' || event.route.id === '/recetas') {
@@ -21,3 +31,12 @@ export const handleError: HandleServerError = async ({ event, error }) => {
 		return { message: MESSAGES.SERVER_ERROR };
 	}
 };
+
+export const handle: Handle = async ({ event, resolve }) => {
+    const response = await resolve(event);
+    Object.entries(securityHeaders).forEach(
+        ([header, value]) => response.headers.set(header, value)
+    );
+
+    return response;
+}
