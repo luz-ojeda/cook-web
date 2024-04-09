0ada57f59ab9bc4901b31c3d1244c7454d6d98c6
Luz <luzojeda@proton.me>
2024-04-09 15:54:59 -0300

    Update RecipesSearchForm

--- src/lib/components/RecipesSearchForm.svelte
+++ src/lib/components/RecipesSearchForm.svelte
@@ -80,7 +80,7 @@
 	<label for="vegetarian">Solo vegetarianas</label>
 
 	{#if $page.url.pathname !== '/'}
-		<PrimaryButton disabled={$recipes.loading} {loading} onClick={onButtonClick} width="100%">
+		<PrimaryButton {loading} onClick={onButtonClick} width="100%">
 			Buscar
 		</PrimaryButton>
 	{:else}
