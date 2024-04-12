6be49b76d6d2d61a99eeab5b7fe5605d2b92e2e3
Luz <luzojeda@proton.me>
2024-04-12 15:44:36 -0300

    Remove uuid package

--- src/routes/admin/crear-receta/+page.server.ts
+++ src/routes/admin/crear-receta/+page.server.ts
@@ -70,16 +70,13 @@ export const actions = {
 async function uploadImage(recipeImage: File, blobName: string) {
 	const sasToken = createSasToken();
 	const blobServiceClient = new BlobServiceClient(
-		`https://${env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net?${sasToken}`,
-		undefined
+		`https://${env.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net?${sasToken}`
 	);
 	const containerClient = blobServiceClient.getContainerClient('recipes');
 	const blockBlobClient = containerClient.getBlockBlobClient(blobName);
 
 	try {
-		await blockBlobClient.uploadData(Buffer.from(await recipeImage.arrayBuffer()), {
-			blobHTTPHeaders: { blobContentType: 'image/jpeg' }
-		});
+		await blockBlobClient.uploadData(Buffer.from(await recipeImage.arrayBuffer()));
 	} catch (error) {
 		console.error(`An error happened while trying to upload the recipe image: ${error}`);
 	}
@@ -90,7 +87,7 @@ function createSasToken() {
 		services: AccountSASServices.parse('b').toString(),
 		resourceTypes: AccountSASResourceTypes.parse('co').toString(),
 		permissions: AccountSASPermissions.parse('w'),
-		protocol: SASProtocol.HttpsAndHttp,
+		protocol: SASProtocol.Https,
 		expiresOn: new Date(new Date().valueOf() + 3 * 60 * 1000) // 3 minutes
 	};
 
