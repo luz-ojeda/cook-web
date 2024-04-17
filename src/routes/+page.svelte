29bcd2e33aeebd52efc0477dda4632f550865a3a
Luz <luzojeda@proton.me>
2024-04-17 14:58:13 -0300

    Add typewriter effect to hero text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,41 @@
 	import { HomeSearch, RecipeCard } from '$lib';
 	import type { PaginatedList } from '$lib/types/PaginatedList';
 	import type { Recipe } from '$lib/types/Recipe';
+	import { onMount } from 'svelte';
+	import { browser } from "$app/environment";
 
 	export let data: PaginatedList<Recipe>;
+
+	const heroAnimationSessionStorageKey = 'userSawHomeHeroAnimation';
+	let heroTitle: string[] = [];
+	const adjectives = ['sencillas', 'simples', 'sin vueltas.'];
+
+	const userSawAnimation = browser
+		? sessionStorage.getItem(heroAnimationSessionStorageKey)
+		: false;
+
+	function animateHeroTitle() {
+		const delay = 100;
+		let totalDelay = 0;
+
+		adjectives.forEach((adjective, index) => {
+			for (let i = 0; i < adjective.length; i++) {
+				setTimeout(() => (heroTitle = [...heroTitle, adjective[i]]), totalDelay + i * delay);
+			}
+			totalDelay += adjective.length * delay + 600;
+
+			if (index !== adjectives.length - 1)
+				setTimeout(() => {
+					heroTitle.length = 0; // Clear the heroTitle array
+				}, totalDelay);
+		});
+
+		sessionStorage.setItem(heroAnimationSessionStorageKey, 'true');
+	}
+
+	onMount(() => {
+		if (!userSawAnimation) animateHeroTitle();
+	});
 </script>
 
 <svelte:head>
@@ -13,7 +46,14 @@
 <!-- Hero -->
 <div class="spacing hero flex-center">
 	<div>
-		<h1>Recetas simples</h1>
+		<h1>
+			{#if !userSawAnimation}
+				Recetas
+				{heroTitle.join('')}
+			{:else}
+				Recetas simples
+			{/if}
+		</h1>
 		<p>
 			Descubrí recetas auténticas y deliciosas, sin anuncios molestos. Tu experiencia culinaria
 			merece ser ininterrumpida. Busca según ingredientes, dificultad y nombre:
@@ -101,7 +141,6 @@
 		@media (max-width: $tabletBreakpoint) {
 			grid-template-columns: repeat(2, minmax(0, 1fr));
 		}
-
 	}
 
 	.see-more {
