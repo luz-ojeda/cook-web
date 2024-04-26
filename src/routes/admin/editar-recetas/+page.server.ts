e0f5c036828e5fbda1fc495d8544bd0a49c12a04
Luz <luzojeda@proton.me>
2024-04-26 11:25:12 -0300

    Use 30 as limit in edit recipes page

--- src/routes/admin/editar-recetas/+page.server.ts
+++ src/routes/admin/editar-recetas/+page.server.ts
@@ -7,7 +7,7 @@ export const load: PageServerLoad<PaginatedList<Recipe>> = async ({ fetch, url }
 	const page = url.searchParams.has('pagina') ? `&page=${url.searchParams.get('pagina')}` : '';
 	const name = url.searchParams.has('nombre') ? `&name=${url.searchParams.get('nombre')}` : '';
 
-	const res = await fetch(`${env.API_URL}/recipes?limit=5` + page + name);
+	const res = await fetch(`${env.API_URL}/recipes?limit=30` + page + name);
 	const responseJson = await res.json();
 	if ('status' in responseJson) {
 		throw new Error(responseJson.title);
