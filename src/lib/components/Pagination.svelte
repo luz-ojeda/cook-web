68debb3dde66144cc6b3787ebca8889a511dd9bc
Luz <luzojeda@proton.me>
2024-06-08 23:25:55 -0300

    Mobile CSS for pagination per page

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -20,8 +20,8 @@
 	}
 </script>
 
-<nav class="flex-center" style="height: 48px">
-	<div>
+<nav>
+	<div class="per-page-container">
 		<label for="per-page">Resultados por página</label>
 		<select
 			name="per-page"
@@ -33,66 +33,77 @@
 			<option selected={perPage == 27} value="27">27</option>
 		</select>
 	</div>
-	<!-- Previous page button -->
-	{#if currentPage !== 1 && totalPages >= 2}
-		<button on:click={() => onPageClick(currentPage - 1)} style="width: 96px;">Pág. anterior</button
-		>
-	{/if}
-
-	<!-- Page 1 button -->
-	<button
-		class={currentPage === 1 ? 'active-page' : ''}
-		disabled={currentPage === 1}
-		on:click={() => onPageClick(1)}>1</button
-	>
-
-	<!-- Rest of pages buttons -->
-	{#if totalPages > PAGES_TO_DISPLAY_IN_BETWEEN + 2}
-		{#if currentPage >= 1 + PAGES_TO_DISPLAY_IN_BETWEEN}
-			<span>...</span>
+	<div class="flex-center">
+		<!-- Previous page button -->
+		{#if currentPage !== 1 && totalPages >= 2}
+			<button on:click={() => onPageClick(currentPage - 1)} style="width: 96px;"
+				>Pág. anterior</button
+			>
 		{/if}
 
-		{#each pagesArray.slice(pagesSliceIndex, pagesSliceIndex + 3) as i}
-			<button
-				class={currentPage === i ? 'active-page' : ''}
-				disabled={currentPage === i}
-				on:click={() => onPageClick(i)}>{i}</button
-			>
-		{/each}
-
-		{#if currentPage <= totalPages - PAGES_TO_DISPLAY_IN_BETWEEN}
-			<span>...</span>
-		{/if}
-	{:else}
-		{#each Array.from(Array(totalPages - 2)).keys() as i}
-			<button
-				class={currentPage === i + 2 ? 'active-page' : ''}
-				disabled={currentPage === i + 2}
-				on:click={() => onPageClick(i + 2)}>{i + 2}</button
-			>
-		{/each}
-	{/if}
-
-	<!-- Last page button -->
-	<button
-		class={currentPage === totalPages ? 'active-page' : ''}
-		disabled={currentPage === totalPages}
-		on:click={() => onPageClick(totalPages)}>{totalPages}</button
-	>
-
-	<!-- Next page button -->
-	{#if currentPage !== totalPages && totalPages > 2}
-		<button on:click={() => onPageClick(currentPage + 1)} style="width: 96px;"
-			>Pág. siguiente</button
+		<!-- Page 1 button -->
+		<button
+			class={currentPage === 1 ? 'active-page' : ''}
+			disabled={currentPage === 1}
+			on:click={() => onPageClick(1)}>1</button
 		>
-	{/if}
+
+		<!-- Rest of pages buttons -->
+		{#if totalPages > PAGES_TO_DISPLAY_IN_BETWEEN + 2}
+			{#if currentPage >= 1 + PAGES_TO_DISPLAY_IN_BETWEEN}
+				<span>...</span>
+			{/if}
+
+			{#each pagesArray.slice(pagesSliceIndex, pagesSliceIndex + 3) as i}
+				<button
+					class={currentPage === i ? 'active-page' : ''}
+					disabled={currentPage === i}
+					on:click={() => onPageClick(i)}>{i}</button
+				>
+			{/each}
+
+			{#if currentPage <= totalPages - PAGES_TO_DISPLAY_IN_BETWEEN}
+				<span>...</span>
+			{/if}
+		{:else}
+			{#each Array.from(Array(totalPages - 2)).keys() as i}
+				<button
+					class={currentPage === i + 2 ? 'active-page' : ''}
+					disabled={currentPage === i + 2}
+					on:click={() => onPageClick(i + 2)}>{i + 2}</button
+				>
+			{/each}
+		{/if}
+
+		<!-- Last page button -->
+		<button
+			class={currentPage === totalPages ? 'active-page' : ''}
+			disabled={currentPage === totalPages}
+			on:click={() => onPageClick(totalPages)}>{totalPages}</button
+		>
+
+		<!-- Next page button -->
+		{#if currentPage !== totalPages && totalPages > 2}
+			<button on:click={() => onPageClick(currentPage + 1)} style="width: 96px;"
+				>Pág. siguiente</button
+			>
+		{/if}
+	</div>
 </nav>
 
 <style lang="scss">
 	@import '../../sass/colors.scss';
+	@import '../../sass/variables.scss';
 	nav {
-		*:not(:last-child) {
-			margin-right: 6px;
+		align-items: center;
+		display: flex;
+
+		@media (max-width: $tabletBreakpoint) {
+			flex-direction: column-reverse;
+
+			*:not(:last-child) {
+				margin-right: 0px;
+			}
 		}
 	}
 
@@ -111,5 +122,24 @@
 	.active-page {
 		background-color: $darkPrimaryColor;
 		color: $grey100;
+	}
+
+	.per-page-container {
+		margin-right: 24px;
+
+		@media (max-width: $tabletBreakpoint) {
+			align-items: center;
+			display: flex;
+			justify-content: center;
+			margin-right: 0px;
+			margin-top: 24px;
+		}
+	}
+
+	select {
+		@media (max-width: $tabletBreakpoint) {
+			height: 48px;
+			width: 48px;
+		}
 	}
 </style>
