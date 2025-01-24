de844533287f270cbf95137e3985b452cd617e65
synthopia <luzojeda@proton.me>
2025-01-24 17:14:13 -0300

    Update +page.svelte pronouns yo

--- src/routes/sobre-recetas-simples/+page.svelte
+++ src/routes/sobre-recetas-simples/+page.svelte
@@ -1,7 +1,7 @@
 <div class="narrow-page">
 	<h1>Recetas Simples</h1>
 	<p>
-		<strong>Hola!</strong> Mi nombre es Luz Ojeda y soy la creadora de la página. Empezó como una idea en
+		<strong>Hola!</strong> Mi nombre es Lucas Ezequiel Ojeda y creé esta página. Empezó como una idea en
 		diciembre de 2023 después de unos meses de que empecé a aprender cocina más "seriamente" con el libro
 		The Food Lab de J. Kenji López-Alt. Durante toda mi vida disfruté cocinar pero nunca me tomé el tiempo
 		y la atención necesaria para aprender los por qué y cómos detrás de esta actividad que para algunos
@@ -9,7 +9,7 @@
 		vez y una pasión.
 	</p>
 	<p>
-		De no encontrarme satisfecha con la mayoría de las páginas web de cocina en español que hay
+		De no encontrarme satisfecho con la mayoría de las páginas web de cocina en español que hay
 		actualmente decidí combinar dos actividades que me apasionan como es el desarrollo web y la
 		cocina y hacer una propia página de recetas. Una que no tuviera demasiada publicidad invasiva,
 		fuera sencilla en diseño y tuviera la información justa y necesaria en cada receta.
